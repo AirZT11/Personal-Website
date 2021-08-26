13f8ed125b891bd18b5a23b77c41c09a5b07cfe5
airzt11 <airzonatea@gmail.com>
2021-08-26 15:36:52 -0400

    reset deployment

--- next.config.js
+++
@@ -1,6 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  // Use the prefix in production and not development.
-  assetPrefix: isProd ? 'https://github.com/AirZT11/airzt11.github.io.git' : ''
-}
